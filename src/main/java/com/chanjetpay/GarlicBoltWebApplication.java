3686ce29667338fbc22454d251791ec6e211e0a3
baolinet <bao.li.net@gmail.com>
2017-11-08 10:59:13 +0800

    init

---
+++ src/main/java/com/chanjetpay/GarlicBoltWebApplication.java
@@ -0,0 +1,12 @@
+package com.chanjetpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarlicBoltWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarlicBoltWebApplication.class, args);
+	}
+}
---
+++ src/main/java/com/chanjetpay/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.chanjetpay;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GarlicBoltWebApplication.class);
+	}
+
+}
