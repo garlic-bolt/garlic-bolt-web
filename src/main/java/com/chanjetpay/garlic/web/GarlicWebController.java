8e27f0e632754a10df37451b0ffecce6a0d9c477
baolinet <bao.li.net@gmail.com>
2017-12-11 22:30:14 +0800

    公众号相关实体

--- src/main/java/com/chanjetpay/garlic/web/GarlicWebController.java
+++ src/main/java/com/chanjetpay/garlic/web/GarlicWebController.java
@@ -1,7 +1,9 @@
 package com.chanjetpay.garlic.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by libaoa on 2017/11/8.
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GarlicWebController {
 
 	@RequestMapping({"/",""})
-	public String index(){
-		return "index1";
+	public String index(Model model){
+		model.addAttribute("link1","http://www.baidu.com");
+		return "index";
 	}
 }
